a041189a9dd00201173476e9de86e5d946f39372
MishB1 <88148320+MishB1@users.noreply.github.com>
2023-10-25 22:04:52 +0000

    small progress

--- lib/features/daily_news/domain/usecases/get_article.dart
+++ lib/features/daily_news/domain/usecases/get_article.dart
@@ -1,6 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:udnews_app/core/resources/data_state.dart';
 import 'package:udnews_app/features/daily_news/domain/entities/article.dart';
 import 'package:udnews_app/features/daily_news/domain/repository/article_repository.dart';
+import 'package:udnews_app/features/daily_news/presentation/bloc/article/remote/remote_artice_state.dart';
 import '../../../../core/usecase/usecase.dart';
 
 class GetArticleUseCase implements UseCase<DataState<List<ArticleEntity>>,void> {
@@ -13,4 +15,16 @@ class GetArticleUseCase implements UseCase<DataState<List<ArticleEntity>>,void> 
    return _articleRepository.getNewsArticles();
   }
 
-}   
+}
+
+class RemoteArticlesLoading extends RemoteArticleState {
+  const RemoteArticlesLoading();
+}
+
+class RemoteArticlesDone extends RemoteArticleState {
+  const RemoteArticlesDone(List<ArticleEntity> article) :super(articles: article);
+}
+
+class RemoteArticlesError extends RemoteArticleState {
+  const RemoteArticlesError(DioException error) :super(error: error);
+}
---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_artice_state.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+import 'package:udnews_app/features/daily_news/domain/entities/article.dart';
+
+
+abstract class RemoteArticleState extends Equatable {
+  final List<ArticleEntity> ? articles;
+  final DioException ? error;
+
+  const RemoteArticleState({this.articles, this.error});
+
+  @override
+  List<Object?> get props => [articles!, error!];
+}
