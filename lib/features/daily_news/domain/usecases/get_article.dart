e79b7199d2fa1b0ba7e7ba434bf4c91998cc5f7f
MishB1 <88148320+MishB1@users.noreply.github.com>
2023-11-07 10:14:45 +0000

    initial dependency injection

--- lib/features/daily_news/domain/usecases/get_article.dart
+++ lib/features/daily_news/domain/usecases/get_article.dart
@@ -17,14 +17,3 @@ class GetArticleUseCase implements UseCase<DataState<List<ArticleEntity>>,void> 
 
 }
 
-class RemoteArticlesLoading extends RemoteArticleState {
-  const RemoteArticlesLoading();
-}
-
-class RemoteArticlesDone extends RemoteArticleState {
-  const RemoteArticlesDone(List<ArticleEntity> article) :super(articles: article);
-}
-
-class RemoteArticlesError extends RemoteArticleState {
-  const RemoteArticlesError(DioException error) :super(error: error);
-}
--- lib/features/daily_news/presentation/bloc/article/remote/remote_artice_state.dart
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_artice_state.dart
@@ -3,12 +3,24 @@ import 'package:equatable/equatable.dart';
 import 'package:udnews_app/features/daily_news/domain/entities/article.dart';
 
 
-abstract class RemoteArticleState extends Equatable {
+abstract class RemoteArticlesState extends Equatable {
   final List<ArticleEntity> ? articles;
   final DioException ? error;
 
-  const RemoteArticleState({this.articles, this.error});
+  const RemoteArticlesState({this.articles, this.error});
 
   @override
   List<Object?> get props => [articles!, error!];
+}
+
+class RemoteArticlesLoading extends RemoteArticlesState {
+  const RemoteArticlesLoading();
+}
+
+class RemoteArticlesDone extends RemoteArticlesState {
+  const RemoteArticlesDone(List<ArticleEntity> article) :super(articles: article);
+}
+
+class RemoteArticlesError extends RemoteArticlesState {
+  const RemoteArticlesError(DioException error) :super(error: error);
 }
